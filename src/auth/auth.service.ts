49b19ee1ec2e96495c81176aaedde87d2a24c7fc
artemgoryanskiy <artem.goryanskiy@gmail.com>
2025-07-16 22:00:16 +0300

    Initialize project with authentication and user modules, add necessary dependencies.

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class AuthService {
+  constructor(@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>) {}
+  async register(dto: any) {
+    return this
+  }
+}
