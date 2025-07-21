2f76be7a2d396c091966dc9f3176e3aafbc7671d
artemgoranskij <artemgoryanskiy@yandex.ru>
2025-07-21 15:30:38 +0300

    Initialize NestJS project with basic structure and configurations
    - Set up project structure with `app` and `auth` modules.
    - Add `.gitignore` for common Node.js and IDE files.
    - Configure Prettier and ESLint.
    - Implement basic `AppController` with root route returning "Hello World".
    - Include unit and e2e tests.
    - Add dependencies and scripts in `package.json`.

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoDbConfig } from './config/mongoose.config';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoDbConfig,
+    }),
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
