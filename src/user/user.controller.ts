7a30d6b06dea6bf5185680cb7a33688f49519649
artemgoryanskiy <artem.goryanskiy@gmail.com>
2025-07-23 20:45:17 +0300

    Extend user management and add admin operations
    - Add routes in `UserController` for admin: user listing, deletion, and detail fetching.
    - Implement `updateProfile` and `updateUser` endpoints for profile updates.
    - Introduce `UpdateUserDto` for input validation.
    - Add `IdValidationPipe` for validating MongoDB IDs.
    - Enhance `UserService` with user count retrieval, filtering, and deletion logic.
    - Update `.prettierrc` formatting configuration.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Put,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { Auth } from '../auth/decorators/auth.decorator';
 import { CurrentUser } from './decorators/user.decorator';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import { IdValidationPipe } from '../pipes/id.validation.pipe';
 
 @Controller('user')
 export class UserController {
@@ -11,5 +24,52 @@ export class UserController {
   @Auth()
   async getProfile(@CurrentUser('_id') _id: string) {
     return this.userService.byId(_id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put('profile')
+  @HttpCode(200)
+  @Auth()
+  async updateProfile(
+    @CurrentUser('_id') _id: string,
+    @Body() dto: UpdateUserDto,
+  ) {
+    return this.userService.updateProfile(_id, dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put(':id')
+  @HttpCode(200)
+  @Auth('admin')
+  async updateUser(
+    @Param('id', IdValidationPipe) id: string,
+    @Body() dto: UpdateUserDto,
+  ) {
+    return this.userService.updateProfile(id, dto);
+  }
+
+  @Get('count')
+  @Auth('admin')
+  async getUserCount() {
+    return this.userService.getUserCount();
+  }
+
+  @Get()
+  @Auth('admin')
+  async getUsers(@Query('searchTerm') searchTerm?: string) {
+    return this.userService.getUsers(searchTerm);
+  }
+
+  @Get(':id')
+  @Auth('admin')
+  async getUser(@Param('id', IdValidationPipe) id: string) {
+    return this.userService.byId(id);
+  }
+
+  @Delete(':id')
+  @HttpCode(200)
+  @Auth('admin')
+  async deleteUser(@Param('id', IdValidationPipe) id: string) {
+    return this.userService.deleteUser(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './user.schema';
 import { Model } from 'mongoose';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import { genSalt, hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -13,5 +15,44 @@ export class UserService {
     const user = await this.userModel.findById(_id);
     if (!user) throw new NotFoundException('User not found');
     return user;
+  }
+
+  async updateProfile(_id: string, dto: UpdateUserDto) {
+    const user = await this.byId(_id);
+    const isSameUser = await this.userModel.findOne({ email: dto.email });
+    if (isSameUser && String(_id) !== String(isSameUser._id))
+      throw new NotFoundException('User with this email already exists');
+
+    if (dto.password) {
+      const salt = await genSalt();
+      user.password = await hash(dto.password, salt);
+    }
+
+    user.email = dto.email;
+    if (dto.isAdmin || dto.isAdmin === false) user.isAdmin = dto.isAdmin;
+    await user.save();
+    return;
+  }
+
+  async getUserCount() {
+    return await this.userModel.find().countDocuments().exec();
+  }
+
+  async getUsers(searchTerm?: string) {
+    let options = {};
+    if (searchTerm) {
+      options = {
+        $or: [{ email: new RegExp(searchTerm, 'i') }],
+      };
+    }
+    return this.userModel
+      .find(options)
+      .select('-password -updatedAt -__v')
+      .sort({ createdAt: 'desc' })
+      .exec();
+  }
+
+  async deleteUser(id: string) {
+    return this.userModel.findByIdAndDelete(id).exec();
   }
 }
